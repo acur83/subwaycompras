8dba8ed2a4195bd8933a192b513c92a862d771c0
Idelis Gé Ramírez <idelis.ge@gmail.com>
2019-04-16 13:10:54 -0400

    [STEP] Try with a folder.

---
+++ cPurchase/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "cPurchase",
+    'summary': """
+        cPurchase""",
+    'author': "SimpleIT",
+    'website': "http://simpleit.com",
+    'category': 'account_invoicing',
+    'depends': ['purchase', 'account'],
+    'data': [
+        'views/model_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# 'security/ir.model.access.csv',
